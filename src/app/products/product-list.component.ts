eed8181bed4d983a2c50b5445aa0de443da458da
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-02 17:15:48 +0100

    new data binding and component data

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
 
+  products: any[] = [{
+    'productId': 1,
+    'productName': 'Product 1',
+    'productCode': 'Code',
+    'releaseDate': '2019-11-01',
+    'price': 50,
+    'starRating': 3,
+    'imageUrl': ''
+  }] ;
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
 
