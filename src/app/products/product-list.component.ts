4ac82f96747593d9621c498f4a44ed6e29b59ff7
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-02 21:07:06 +0100

    manage events from child to parent and viceversa

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -59,5 +59,9 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     console.log('In OnInit');
   }
+
+  onRatingClick(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 }
 
