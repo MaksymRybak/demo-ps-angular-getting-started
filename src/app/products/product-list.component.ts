eb33c52272202ba7189d6b20a0ea68ad6b366901
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-02 18:27:30 +0100

    new pipe and hook

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -15,16 +16,20 @@ export class ProductListComponent {
   products: IProduct[] = [{
     productId: 1,
     productName: 'Product 1',
-    productCode: 'Code',
+    productCode: 'Code-1',
     releaseDate: '2019-11-01',
     price: 50,
-    description: '',
+    description: 'Description 1...',
     starRating: 3,
     imageUrl: ''
   }] ;
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
   }
 }
 
