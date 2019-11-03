2d97b1a8bc1550666145cc2a4fabeead21c07d5d
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-03 15:20:03 +0100

    read path parameter using Router service

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  pageTitle = 'Product detail';
+  product: IProduct;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private router: Router) {
 
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
+
+  ngOnInit(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.pageTitle += ` ${id}`;
+    this.product = {
+      productId: id,
+      productName: 'Leaf Rake',
+      productCode: 'GDN-0011',
+      releaseDate: 'March 19, 2019',
+      description: 'Leaf rake with 48-inch wooden handle.',
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl: 'assets/images/leaf_rake.png'
+    };
+  }
 }
