69e229cdb8075976f3286fbe3ae74678e054662e
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-03 14:21:59 +0100

    base routing settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div>
-    <h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>`
+  template: `
+    <nav class="navbar navbar-expand bg-light">
+      <a class="navbar-brand">{{pageTitle}}</a>
+      <ul class="nav nav-pills">
+        <li class="nav-link" [routerLink]="['./']">Home</li>
+        <li class="nav-link" [routerLink]="['./products']">Product List</li>
+      </ul>
+    </nav>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+  `
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `<div>
+    Welcome page....
+  </div>`
+})
+export class WelcomeComponent {
+
+}
