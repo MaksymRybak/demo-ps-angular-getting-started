88a6d8ce4baa8d13b78d27879715b14f2e3d131a
Maksym Rybak <rybak.maksym@gmail.com>
2019-11-03 11:54:39 +0100

    get data using HttpClient service from angular http client module

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,29 +1,36 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  getProducts(): IProduct[] {
-    return [{
-      productId: 1,
-      productName: 'Product 1',
-      productCode: 'Code-1',
-      releaseDate: '2019-11-01',
-      price: 50,
-      description: 'Description 1...',
-      starRating: 3,
-      imageUrl: ''
-    }, {
-      productId: 2,
-      productName: 'Product 2',
-      productCode: 'Code-2',
-      releaseDate: '2019-11-02',
-      price: 50,
-      description: 'Description 2...',
-      starRating: 4,
-      imageUrl: ''
-    }];
+  private productUrl = 'api/products/products.json';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.httpClient.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // TODO send error message to remote logging infrastructure
+    let errorMsg = '';
+    if (err.error instanceof ErrorEvent) {
+      // client side or network error
+      errorMsg = `An error occured: ${err.error.message}`;
+    } else {
+      // BE returned unsuccessful response code 
+      errorMsg = `Server returned ${err.status}, error message is ${err.message}`;
+    }
+    console.log(errorMsg);
+    return throwError(errorMsg);
   }
 }
